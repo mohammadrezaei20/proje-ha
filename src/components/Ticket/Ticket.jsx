b94d854995d85b3bfeaa76944870be4a533bc54f
Mohammad <mohammadrezaei2004@gmail.com>
2024-09-26 18:12:44 +0330

    COMMIT

--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -10,49 +10,71 @@ export default class Ticket extends React.Component {
             countriesData: {
                 Iran: ["Tabriz", "Tehran", "Shiraz", "Esfahan", "Mashhad"],
                 Turkey: ["Istanbul", "Ezmir", "Ankara", "Antaliya"],
-                US: ["Los Angles", "San Diego", "Chicago"],
+                US: ["Los Angeles", "San Diego", "Chicago"],
             },
-            mainCountryCities: [
-
-            ]
+            mainCountryCities: []
         }
+        this.selectHandler = this.selectHandler.bind(this)
     }
 
+    selectHandler(event) {
+        const mainCountry = event.target.value;
+
+        if (mainCountry === "-1") {
+            this.setState({ mainCountryCities: [] });
+        } else {
+            const mainCountryCities = this.state.countriesData[mainCountry];
+            this.setState({ mainCountryCities });
+        }
+    }
 
     render() {
         return (
             <div className="container">
-                <div className="col-md- box">
-                    <input className="fnameInput" placeholder="First Name" />
+
+                <div className=" col-md-6  box">
+                    <input className="fnameInput form-control" placeholder="First Name" />
                 </div>
 
-                <div className="col-md- box">
-                    <input className="lnameInput" placeholder="Last Name" />
+                <div className=" col-md-6  box">
+                    <input className="lnameInput form-control" placeholder="Last Name" />
                 </div>
-                <div className="col-md- box">
-                    <input className="phoneInput" placeholder="Phone Number" />
+
+                <div className=" col-md-6  box">
+                    <input className="phoneInput form-control" placeholder="Phone Number" />
                 </div>
-                <div className="col-md- box">
-                    <input className="emailInput" placeholder="Email" />
+
+                <div className=" col-md-6  box">
+                    <input className="emailInput form-control" placeholder="Email" />
                 </div>
-                <div className="col-md- box">
-                    <select className="countrySelect">
+
+                <div className=" col-md-6  box">
+                    <select className="countrySelect form-control" onChange={this.selectHandler}>
                         <option value="-1">Please Select ...</option>
                         <option className="option" value="Iran">Iran</option>
                         <option className="option" value="Turkey">Turkey</option>
-                        <option className="option" value="US">United State</option>
+                        <option className="option" value="US">United States</option>
                     </select>
                 </div>
-                <div className="col-md-6 box">
-                    <select className="citySelect">
+
+                <div className="col-lg-6 col-md-6 col-sm-12 box">
+                    <select className="citySelect form-control">
+                        {this.state.mainCountryCities.length ?
+                            this.state.mainCountryCities.map((city, index) => (
+                                <option key={index} value={city}>
+                                    {city}
+                                </option>
+                            )) :
                             <option value="-1">Please Select City</option>
+                        }
                     </select>
                 </div>
+
                 <div className="col-md-12 box">
-                    <button className="btn">Book a ticket</button>
+                    <button className="btn btn-primary">Book a ticket</button>
                 </div>
             </div>
 
-        )
+        );
     }
 }
